1482fb182f6add8def302dd73cbca6abf23f69a0
ybapfer <ydvbfermin@minha.fag.edu.br>
2025-04-25 21:05:34 -0300

    Update prova.java

--- Alunos/YISOLHET/prova_1tri/prova.java
+++ Alunos/YISOLHET/prova_1tri/prova.java
@@ -1,1 +1,31 @@
+package eventos;
+
+public class Cliente {
+	
+	private String nome;
+	private String email;
+	
+	public Cliente(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+		
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	
+		
+	 @Override
+	    public String toString() {
+	        return nome + " (" + email + ")";
+	    }
+
+}
 
